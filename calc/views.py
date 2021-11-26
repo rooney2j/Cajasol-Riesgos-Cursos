940e33e5c99fd9307c9af8f3998107dde6f46ffd
Ronnie Santoyo <rsantoyo@unsa.edu.pe>
2021-11-26 08:19:41 -0500

    Hasta el min 1h21m

--- calc/views.py
+++ calc/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {'nombre': 'Ronnie'})
+
+def add(request):
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+
+    res = val1 + val2
+
+    return render(request, 'resultado.html', {'resultado': res});
